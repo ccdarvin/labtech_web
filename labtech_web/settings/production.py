bb2537a232cef5a75762ea02fa6c95800b722b1a
Darvin <cc.darvin@gmail.com>
2021-04-23 16:22:15 -0500

    type correction  in environment variables

--- labtech_web/settings/production.py
+++ labtech_web/settings/production.py
@@ -2,8 +2,8 @@ from .base import *
 import environ
 env = environ.Env()
 
-SECRET_KEY = env.db('SECRET_KEY')
-ALLOWED_HOSTS = env.db('ALLOWED_HOSTS')
+SECRET_KEY = env('SECRET_KEY')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 DEBUG = env.bool('DEBUG', True)
 
 if 'DATABASE_URL' in env:
@@ -12,11 +12,11 @@ if 'DATABASE_URL' in env:
     DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = env.db('AWS_S3_REGION_NAME')
-AWS_S3_ENDPOINT_URL = env.db('AWS_S3_ENDPOINT_URL')
-AWS_ACCESS_KEY_ID = env.db('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env.db('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env.db('AWS_STORAGE_BUCKET_NAME') 
+AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
+AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME') 
 
 # try:
 #     from .local import *
